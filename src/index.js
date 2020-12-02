06f74a57bfb5d9e1a7e8b8a5beb71bdcac586437
JeremT8 <jeremytilletpro@gmail.com>
2020-12-02 14:51:04 +0100

    pushons

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'reset-css';
+import Vue from 'vue';
+import App from 'App.vue';
+
+console.log(window);
+new Vue({
+  el: '#example',
+  data: {
+    slides: 7
+  },
+  components: {
+    'carousel-3d': window['carousel-3d'].Carousel3d,
+    'slide': window['carousel-3d'].Slide
+  }
+})
