5be23f35a1b3b0e171b8b5209a358b2a8b05338c
JeremT8 <jeremytilletpro@gmail.com>
2020-12-02 17:48:24 +0100

    router.js check problem on parcel resolve tomorrow

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import 'reset-css';
 import Vue from 'vue';
 import App from 'App.vue';
-import router from './router';
+import router from 'router';
 
-console.log(window);
+/**console.log(window);
 
 new Vue({
   el: '#example',
@@ -14,4 +14,15 @@ new Vue({
     'carousel-3d': window['carousel-3d'].Carousel3d,
     'slide': window['carousel-3d'].Slide
   }
-})
+});
+*/
+
+
+// on démarre VueJS
+// On utilise App comme composant racine => createElement(App)
+// on monte ce composant racine à la place de l'élément qui porte l'id "app" => $mount('#app')
+// on passe le router à notre instance de VueJS
+new Vue({ 
+  render: createElement => createElement(App),
+  router
+}).$mount('#app');
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,45 @@
 import axios from 'axios';
 
-const apiBaseURL = '';
+const apiBaseURL = 'http://benoit-le-mailloux.vpnuser.lan/projet-o-ctodraw/wp-json';
 
+// on déclare le module
+const userService = {
+    token: null,
+    init() {
+        // si on trouve un token dans le stockage de session, on le stocke dans userService.token
+        if (sessionStorage.getItem('token')) {
+            userService.token = sessionStorage.getItem('token');
+        }
+    },
+    isUserConnected() {
+        // on veut renvoyer un booleen (ici avec une double inversion)
+        return !!userService.token;
+    },
+    registerUser() {
+        
+    },
+    connectUser(username, password) {
+        // on return la promesse d'axios pour permettre au composant qui appelera cette méthode de faire quelque chose lorsque la requête est réussie (avec .then())
+        return axios.post(apiBaseURL + '/jwt-auth/v1/token', {
+            // au lieu de déclarer :
+            //    username: username,
+            //    password: password
+            // on peut raccourcir : 
+            username,
+            password
+        })
+        .then((response) => {
+            // console.log('RESPONSE', response);
+            // on récupère le token renvoyé l'API dans l'objet response
+            userService.token = response.data.data.token;
+            // on stocke le token dans les données de session du navigateur
+            sessionStorage.setItem('token', userService.token);
+        })
+        .catch((error) => {
+            console.log('ERROR', error);
+        });
+   }
+}
 
 // on exporte notre module
 export default userService;
