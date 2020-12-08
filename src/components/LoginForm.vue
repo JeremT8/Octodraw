839dc987a218303c590edec63957d185c429dad5
Wendy-Hyperion <monakine@outlook.fr>
2020-12-08 09:09:18 +0100

    image et form du register

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -36,7 +36,7 @@ export default {
             password: '',
             errors: {
                 username: '',
-                password: ''
+                password: '',
             }
         };
     },
