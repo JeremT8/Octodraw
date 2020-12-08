3fd86c4f3267095fd8ef332a91990976fa7c3b58
Wendy-Hyperion <monakine@outlook.fr>
2020-12-08 15:36:02 +0100

    theme galery ok

--- src/router.js
+++ src/router.js
@@ -14,6 +14,7 @@ import ProfileView from './views/ProfileView';
 import ContactView from './views/ContactView';
 
 import ThemeView from './views/ThemeView';
+import GaleryView from './views/GaleryView';
 
 
 
@@ -31,7 +32,8 @@ const routes = [
   { path: '/profil', component: ProfileView },
   { path: '/contact', component: ContactView },
 
-  { path: '/themes', component: ThemeView }
+  { path: '/themes', component: ThemeView },
+  { path: '/galerie', component: GaleryView },
 
 ];
 
