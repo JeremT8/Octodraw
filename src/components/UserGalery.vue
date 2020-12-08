50a388fe42c200d8500529ba314ee709c2be310f
Wendy-Hyperion <monakine@outlook.fr>
2020-12-08 17:04:55 +0100

    theme user galery ok

--- src/components/UserGalery.vue
+++ src/components/UserGalery.vue
@@ -1,15 +1,101 @@
 <template>
-  <div>
-  
+<div>
+    
+    <div class="images-top">
+        <img class="tentacule" src="../assets/images/tentacules_bleu_octodraw.svg" fluid alt="Responsive image">
+        <img class="tentacule" src="../assets/images/tentacules_rouge_octodraw.svg" fluid alt="Responsive image">
+    </div>
+    
+  <div class="picture">
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+                <b-img src="https://www.beautycolorcode.com/de00a5.png" alt="Image" class="rounded-0"></b-img>
+    </b-card>
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+                <b-img src="https://www.beautycolorcode.com/de00a5.png" alt="Image" class="rounded-0"></b-img>
+    </b-card>
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+                <b-img src="https://www.beautycolorcode.com/de00a5.png" alt="Image" class="rounded-0"></b-img>
+    </b-card>
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+                <b-img src="https://www.beautycolorcode.com/de00a5.png" alt="Image" class="rounded-0"></b-img>
+    </b-card>
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+                <b-img src="https://www.beautycolorcode.com/de00a5.png" alt="Image" class="rounded-0"></b-img>
+    </b-card>
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+                <b-img src="https://www.beautycolorcode.com/de00a5.png" alt="Image" class="rounded-0"></b-img>
+    </b-card>
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+                <b-img src="https://www.beautycolorcode.com/de00a5.png" alt="Image" class="rounded-0"></b-img>
+    </b-card>
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+                <b-img src="https://www.beautycolorcode.com/de00a5.png" alt="Image" class="rounded-0"></b-img>
+    </b-card>
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+                <b-img src="https://www.beautycolorcode.com/de00a5.png" alt="Image" class="rounded-0"></b-img>
+    </b-card>
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+                <b-img src="https://www.beautycolorcode.com/de00a5.png" alt="Image" class="rounded-0"></b-img>
+    </b-card>
+    
+   </div> 
   </div>
 </template>
 
 <script>
+
 export default {
-  
+
 }
+
 </script>
 
 <style lang="scss" scoped>
 
+.theme_define {
+    margin-left: 15%;
+    text-align: center;
+}
+
+.m-1 {
+    
+    width: 49%
+    
+
+}
+
+.overflow-hidden {
+    width: 200px;
+    height: 200px;
+}
+
+.picture {
+    display:flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    border-radius: 15px;
+    background-image: linear-gradient(to right, #ddcdccd7 , #92908f);
+    padding: 40px;
+    margin: 10px 40px;
+}
+
+img {
+    margin-left: 0px;
+    margin-bottom: 0px;
+}
+
+.tentacule {
+    width:300px;
+   
+    
+}
+
+.images-top {
+  display:flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin-bottom: -12px;
+  
+}
 </style>
---
+++ src/views/GaleryView.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- le rôle des views est d'instancier les éléments qui composent une page
+    les views seront instanciées par le routeur -->
+    <main class="main-container">
+        <Galery></Galery>
+    </main>
+</template>
+
+<script>
+import Galery from '../components/Galery';
+export default {
+    components: {
+        Galery
+    }
+}
+
+</script>
