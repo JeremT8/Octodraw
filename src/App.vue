f5d8b40955c02175db7fd97cbfa41e8696500b96
Wendy-Hyperion <monakine@outlook.fr>
2020-12-09 16:59:36 +0100

    header connecté ok

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
-    <Header></Header>
+    <HeaderConnected></HeaderConnected>
+    <!--<Header></Header>-->
     <!-- On prévoit que cette zone (entre header et footer) est la partie dynamique de l'app => selon la page demandée, on affiche un composant différent -->
     <!-- C'est ici que VueRouter instanciera le composant correspondant à la route courante -->
     <router-view></router-view>
@@ -14,6 +15,8 @@
 import Vue from 'vue';
 import Header from  "./components/Header";
 
+import HeaderConnected from "./components/HeaderConnected";
+
 import Footer from "./components/Footer";
 import userService from "./services/UserService"; 
 
@@ -24,6 +27,7 @@ export default Vue.extend({
     };
   },
   components: {
+    HeaderConnected,
     Header,
     Footer,
 
